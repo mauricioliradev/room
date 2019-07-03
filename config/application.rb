0e1baf640d44edf8b0035b0440ac486b6d015524
mauricioliradev <mauricioliraweb@gmail.com>
2019-07-03 19:06:23 -0300

    devise ok with i18

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,15 @@ module Room
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'America/Fortaleza'
+    # Altera o ActiveRecord pra gravar os campos(created/updated_at) com o mesmo time zone da aplicação
+    # config.active_record.default_timezone = :local
 
+    config.active_record.time_zone_aware_types = [:datetime, :time]
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = 'pt-BR'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
